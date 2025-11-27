a40b4a7883e78b0e0c2d7f8679463d2d568782d7
Shibili8 <shibilishibizz99@gmail.com>
2025-11-27 16:42:29 +0530

    Server.js Updated

--- server.js
+++ server.js
@@ -439,7 +439,7 @@ app.post("/pdf/export", async (req, res) => {
                       p.link.startsWith("http")
                         ? p.link
                         : "https://" + p.link
-                    )}" style="color:blue;">${safe(p.link)}</a>`
+                    )}" style="color:blue;" target="_blank">${safe(p.link)}</a>`
                   : ""
               }
             </div>
