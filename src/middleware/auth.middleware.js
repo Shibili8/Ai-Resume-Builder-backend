121effa2ff19827f11ba5334ae045141869992a3
Shibili8 <shibilishibizz99@gmail.com>
2026-04-06 12:18:06 +0530

    auth middleware updated

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,15 +2,15 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/env.js";
 
 export function authMiddleware(req, res, next) {
-  const aiToken = req.headers.authorization?.split(" ")[1];
+  const authToken = req.headers.authorization?.split(" ")[1];
 
   // ❌ No token
-  if (!aiToken) {
+  if (!authToken) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
   try {
-    const decoded = jwt.verify(aiToken, JWT_SECRET);
+    const decoded = jwt.verify(authToken, JWT_SECRET);
 
     req.user = decoded; // attach user
     next();
